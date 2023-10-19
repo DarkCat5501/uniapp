2cd35cfd0fb2134c41aa12bcb8a1622699258632
DarkCat5501 <viccente512@gmail.com>
2023-10-19 19:06:29 -0300

    WIP: restruturadas páginas e componentes

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-import {
-  include,
-  loadScripts,
-  stripScripts,
-  toElements,
-} from "./scripts/nilla.js";
+import { include, toElement } from "./scripts/nilla.js";
 import { router } from "./scripts/router.js";
 
 window.onload = async function() {
-  console.log("started");
-  router.load();
-
-  const app = document.getElementById("app");
-
-  const navElement = await include("./pages/navigation.html");
-  const navBar = toElements(navElement);
-  const navScripts = stripScripts(navBar);
-
-  loadScripts(navScripts);
-  app.insert(navBar);
+  const pageData = await include(router.load());
+  const page = toElement(pageData);
+  app.insert(page);
 };
--- scripts/router.js
+++ scripts/router.js
@@ -2,15 +2,26 @@ export class Router {
   constructor(routes) {
     this.routes = routes;
   }
-
   get params() {
     const params = new URLSearchParams(window.location.search);
+
     return params;
   }
 
+  get page() {
+    return this.params.page;
+  }
+
   load() {
-    console.log(this.params);
+    const page = this.page;
+    if (page && page in this.routes) {
+      return this.routes[page];
+    } else {
+      return "./pages/404.html";
+    }
   }
 }
 
-export const router = new Router();
+export const router = new Router({
+  main: "./pages/main.html",
+});
